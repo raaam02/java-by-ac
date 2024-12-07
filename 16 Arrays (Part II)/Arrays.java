d4992dfe1b63e26b07f333d2477520ebd130797e
raaam02 <kingk9869991@gmail.com>
2024-12-07 23:47:19 +0530

    max sum subarray by kadane's algo & brute force note img

--- 16 Arrays (Part II)/Arrays.java
+++ 16 Arrays (Part II)/Arrays.java
@@ -19,7 +19,7 @@ public class Arrays {
         System.out.println("max sum = " + maxSum);
     }
 
-    // 
+    // 2. Maximum Subarray Sum by Pre Fix - time Comp = O(n2)
     public static void maxSubArraySum_PreFix(int array[]){
         int prefix[] = new int[array.length];
         prefix[0] = array[0];
@@ -39,14 +39,45 @@ public class Arrays {
         }
         System.out.println("Max Sum by preFix = " + maxSum);
     }
+
+    // 3. Maximum Subarray Sum by kadane's Algo - time Comp = O(n)
+    public static void maxSubArraySum_kadanes(int array[]){
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        boolean allNeg = true;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                allNeg = false;
+                break;
+            } else {
+                maxSum = Math.max(maxSum, array[i]);
+            }
+        }
+
+        if (!allNeg) {
+            for (int i = 0; i < array.length; i++) {
+                currSum += array[i];
+                if (currSum < 0) {
+                    currSum = 0;
+                }
+                maxSum = Math.max(maxSum, currSum);
+            }
+        }
+        System.out.println("Max Sum by Kadan's Algo = " + maxSum);
+    }
     public static void main(String[] args) {
-        int array[] = {1,2,3,4,-5};
+        int array[] = {-2,-3,4,-2,-1,5,-3};
 
         // 1. Maximum Subarray Sum by Brut Force(Basic ) - time Comp = O(n3)
         maxSubArraySum_brutForce(array);
 
         // 2. Maximum Subarray Sum by Pre Fix - time Comp = O(n2)
         maxSubArraySum_PreFix(array);
+
+        // 3. Maximum Subarray Sum by kadane's Algo - time Comp = O(n)
+        maxSubArraySum_kadanes(array);
+
 
     }
 }
