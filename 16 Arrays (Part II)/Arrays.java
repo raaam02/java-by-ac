26de951a9039234d62411f95ae22cc57de4b5879
raaam02 <kingk9869991@gmail.com>
2024-12-08 18:15:17 +0530

    kadane's more efficient method & notes of prefix and kadane's algo

--- 16 Arrays (Part II)/Arrays.java
+++ 16 Arrays (Part II)/Arrays.java
@@ -66,8 +66,20 @@ public class Arrays {
         }
         System.out.println("Max Sum by Kadan's Algo = " + maxSum);
     }
+
+    // 3. Kadane's algorithm more efficient and consice
+    public static int maxSubArraySum_Kadane(int[] nums) {
+        int maxSoFar = nums[0];
+        int maxEndingHere = nums[0];
+    
+        for (int i = 1; i < nums.length; i++) {
+            maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
     public static void main(String[] args) {
-        int array[] = {-2,-3,4,-2,-1,5,-3};
+        int array[] = {-2,-3,4,-2,1,-5,3};
 
         // 1. Maximum Subarray Sum by Brut Force(Basic ) - time Comp = O(n3)
         maxSubArraySum_brutForce(array);
@@ -77,6 +89,7 @@ public class Arrays {
 
         // 3. Maximum Subarray Sum by kadane's Algo - time Comp = O(n)
         maxSubArraySum_kadanes(array);
+        System.out.println(maxSubArraySum_Kadane(array));
 
 
     }
